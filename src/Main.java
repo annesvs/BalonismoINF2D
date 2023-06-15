a893918fe87206d2570d4861195727e2f5b18a12
Anna Lu Souza <anna7silvas@gmail.com>
2023-06-15 12:17:47 -0300

    lpap

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Pessoa p1 = new Pessoa();
+        Pessoa p2 = new Pessoa();
+
+        p1.nome = "Anna";
+        p2.nome = "Zayn";
+
+        p1.rua = "Rua 11";
+        p1.cidade = "Barueri";
+        p1.estado = "São Paulo";
+
+        p2.rua = "Rua 11";
+        p2.cidade = "Barueri";
+        p2.estado = "São Paulo";
+    }
+}
